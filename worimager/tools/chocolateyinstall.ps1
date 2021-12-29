40a7ce3f6b7f358ec6854d01218f8d972c03338f
Smart123s <28480228+Smart123s@users.noreply.github.com>
2021-12-29 17:27:18 +0100

    [worimager] Initial commit

---
+++ worimager/tools/chocolateyinstall.ps1
@@ -0,0 +1,46 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.orangedox.com/WoR-Release-2.2.2?dl=1'
+$PackageParameters = Get-PackageParameters
+
+### PARAMETERS
+If ($PackageParameters.InstallDir) {
+  $installDir = $PackageParameters.InstallDir
+} Else {
+  $installDir = "$toolsDir"
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  UnzipLocation = $targetDir = $installDir
+  
+  url           = $url
+  checksum      = 'FF536EDB05CD36A528BA786CBEE7BD55993DB182F98DE7156D43C840EF44DC3A'
+  checksumType  = 'sha256'
+
+}
+
+# Check Windows version (min: 1703)
+if ( [Environment]::OSVersion.Version.Build -lt 16299) {
+  Write-Host "  ** Windows 10 build 1703 or later is required." -ForeGround Red
+  throw
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$targetPath = Join-Path "$installDir" "WoR.exe"
+
+# Add StartMenu shortcut
+If (-Not( $PackageParameters.NoStartMenuShortcut )) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
+  $programsFilePath = Join-Path "$programsPath" "Windows on Raspberry imager.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath" -WorkingDirectory "$installDir"
+}
+
+# Add Desktop shortcut
+If (-Not( $PackageParameters.NoDesktopShortcut )) {
+  $programsPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+  $programsFilePath = Join-Path "$programsPath" "Windows on Raspberry imager.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath" -WorkingDirectory "$installDir"
+}
+
---
+++ worimager/tools/chocolateyuninstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop'
+Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName "WoR_Release_2.2.2.zip"
+
+# Remove start menu shortcut
+$programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
+$programsFilePath = Join-Path $programsPath "Windows on Raspberry imager.lnk"
+if (Test-Path "$programsFilePath") { Remove-Item "$programsFilePath" }
+
+# Remove desktop icon
+$programsPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+$shortcutFilePath = Join-Path "$programsPath" "Windows on Raspberry imager.lnk"
+if (Test-Path "$shortcutFilePath") { Remove-Item "$shortcutFilePath" }
