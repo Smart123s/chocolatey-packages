ee8a8b0549aa52193021c8ffc31d8ef2d5fd5930
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-01-23 20:48:27 +0100

    [tresorit] Initial commit

---
+++ tresorit/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url64         = 'https://installerstorage.blob.core.windows.net/public/install/Tresorit.exe'
+
+  softwareName  = 'Tresorit'
+
+  checksum64    = '5219f1d890332126191f58a1484c631423c68a55503509a47082faf42906599b'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/s /v"/qn"'
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs
