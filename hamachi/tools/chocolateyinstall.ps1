bb43135a1339db46fbaf215fe5179bfc04fb5b46
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-01-23 16:51:50 +0100

    [hamachi] Initial commit

---
+++ hamachi/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$silentArgs            = "/qn /norestart REBOOT=ReallySuppress STARTUI=0 /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url           = 'https://secure.logmein.com/hamachi.msi'
+
+  softwareName  = 'LogMeIn Hamachi'
+
+  checksum      = '67efb9aa0b7ca0166078edd04ea5f09234c11ac45cb00d31367b9fbce290c53f'
+  checksumType  = 'sha256'
+
+  silentArgs    = $silentArgs
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs
