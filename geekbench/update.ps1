b9d5928d0b035433788c6e9d91883f9fe00bbf16
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-01-25 18:51:13 +0100

    [geekbench] Add update.ps1 for metapackage

---
+++ geekbench/update.ps1
@@ -0,0 +1,28 @@
+Import-Module AU
+
+$releases = 'https://www.geekbench.com/download/windows/'
+
+function global:au_SearchReplace {
+    $majorVersion = $($Latest.Version).Split('.') | select -First 1
+    # https://github.com/joachimschmidt557/chocolatey-packages-manual-automatic/blob/e89eed2fa0800092ca395a816b7c98280b7e0d9a/automatic/youtube-dl-gui/update.ps1
+    @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName)$majorVersion`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    # Do not download installer
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    $regex         = '.*-WindowsSetup.exe'
+    $url           = $download_page.links | ? href -match $regex | select -First 1 -expand href
+    $version       = ($url -split '-' | select -Skip 1 -Last 1)
+	
+    return @{ Version = $version; URL64 = $url }
+}
+
+Update-Package -ChecksumFor none
