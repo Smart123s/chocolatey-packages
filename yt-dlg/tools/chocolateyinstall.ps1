ed5e46d9601c3fd0a38c8e0c81a39c2e87282b0d
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-01-25 18:10:04 +0100

    [UNUSED][yt-dlg] Upload package
    This package had been allready submitte by someone else.
    Superior package:
    https://github.com/joachimschmidt557/chocolatey-packages-manual-automatic/pull/17

---
+++ yt-dlg/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
+$silentArgs            = "/qn /norestart REBOOT=ReallySuppress /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  file64        = Join-Path $toolsDir 'yt-dlg.msi'
+  silentArgs    = $silentArgs
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'yt-dlg*'
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item -Force -EA 0 -Path $toolsDir\*.msi
