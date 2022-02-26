f378691ab62722b4f96ad30c54b5d6d30251e4b0
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-02-26 10:14:55 +0100

    [turnoffxboxcontroller] Initial commit

---
+++ turnoffxboxcontroller/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+
+$releases = 'https://github.com/JulyIghor/TurnOffXboxController/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+          "(?i)(Download x86:).*"   = "`${1} $($Latest.URL32)"
+          "(?i)(sha256 x86:).*"        = "`${1} $($Latest.Checksum32)"
+          "(?i)(Download x64:).*"   = "`${1} $($Latest.URL64)"
+          "(?i)(sha256 x64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.FileType = $Latest.URL64 -split '\.' | select -last 1
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url32 = $download_page.links | ? href -match '.exe$' | % href | select -First 1
+    $url64 = $download_page.links | ? href -match '.exe$' | % href | select -First 1 -Skip 1
+
+    $version = ($url32 -split '/' | select -Last 1 -Skip 1).Replace('v','')
+
+    @{
+       URL32   = 'https://github.com' + $url32
+       URL64   = 'https://github.com' + $url64
+       Version = $version
+    }
+}
+
+try {
+    update -ChecksumFor none
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
