7e9498cdfc3641816cfdd8c1dbee83cc0eecccb3
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-02-26 10:43:27 +0100

    [turnoffxboxcontroller] Add shortcuts

---
+++ turnoffxboxcontroller/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Remove start menu shortcut
+$programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Turn Off Xbox 360 Controller.lnk"
+if (Test-Path "$programsPath") { Remove-Item "$programsPath" }
+
+# Remove desktop icon
+$shortcutFilePath = "$Env:USERPROFILE\Desktop\Turn Off Xbox 360 Controller.lnk"
+if (Test-Path "$shortcutFilePath") { Remove-Item "$shortcutFilePath" }
