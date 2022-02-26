8b1d1614de1361a8b481decabe1e1b6717585d78
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-02-26 17:38:14 +0100

    [fmodel] Upload icon

---
+++ fmodel/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+$PackageParameters = Get-PackageParameters
+
+# https://stackoverflow.com/a/25682508/9767089
+Function IIf($If, $Right, $Wrong) {If ($If) {$Right} Else {$Wrong}}
+
+$bits = ( Iif [Environment]::Is64BitOperatingSystem 64 32)
+
+$toolsDir   =	"$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$targetPath = Join-Path "$toolsDir" "Turn.Off.Xbox.360.Controller_x$bits.exe"
+
+# Add StartMenu shortcut
+If (-Not( $PackageParameters.NoStartMenuShortcut )) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Turn Off Xbox 360 Controller.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath"
+}
+
+# Add Desktop shortcut
+If (-Not( $PackageParameters.NoDesktopShortcut )) {
+  $programsPath = "$Env:USERPROFILE\Desktop\Turn Off Xbox 360 Controller.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath"
+}
+
+# Add auto start to registry
+If (-Not( $PackageParameters.NoAutoStart )) {
+  $regPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+  New-ItemProperty -Path "$regPath" -Name "$env:ChocolateyPackageName" -PropertyType String -Value "$targetPath"
+}
