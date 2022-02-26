06e46863778b85b8ee981cdffa03d3c42c4cd8e0
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-02-26 18:48:49 +0100

    [fmodel] Initial commit

---
+++ fmodel/update.ps1
@@ -0,0 +1,36 @@
+import-module au
+
+$releases = 'https://github.com/iAmAsval/FModel/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\VERIFICATION.txt" = @{
+          "(?i)(Download:).*"    = "`${1} $($Latest.URL64)"
+          "(?i)(sha256:).*"      = "`${1} $($Latest.Checksum64)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.FileType = $Latest.URL64 -split '\.' | select -last 1
+    Get-RemoteFiles -Purge -NoSuffix -FileNameBase "FModel"
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url = $download_page.links | ? href -match '.zip$' | % href | select -First 1
+    $version = ($url -split '/' | select -Last 1 -Skip 1).Replace('v','')
+
+    @{
+       URL64   = 'https://github.com' + $url
+       Version = $version
+    }
+}
+
+try {
+    update -ChecksumFor none
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
