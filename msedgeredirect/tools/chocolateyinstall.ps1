dd4080c5b58b790d7a27145d364e05a6e68d7e66
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-02-27 18:26:04 +0100

    [msedgeredirect] Initial commit

---
+++ msedgeredirect/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   =	"$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$configFile = Join-Path $toolsDir 'Setup.ini'
+
+# THIS IS NOT A PORTABLE PACKAGE!!!!
+# CHECK WHAT THE INSTALLER DOES!!!
+# IT MIGHT HANDLE DESKTOP ICONS AS WELL
+# ALSO CHECK UNINSTALL SCRIPT
+#   FOR SHORTCUT HANDLING
+#   AND PROPER UNINSTALLATION
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+
+  file           = Join-Path $toolsDir 'MSEdgeRedirect_x32.exe'
+  checksum       = '2C9489C91212019F4BA74834A1659A7E68A9F204F6A492CA66D2ED28E900105F'
+
+  file64         = Join-Path $toolsDir 'MSEdgeRedirect_x64.exe'
+  checksum64     = 'FDFF47E3974322E8040D8F69DACCE71219C68E0D00097C168A666FE5B69C17DA'
+
+  softwareName   = 'MSEdgeRedirect'
+  checksumType   = 'sha256'
+
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = "/silentinstall $configFile"
+}
+
+Install-ChocolateyPackage @packageArgs
+
+Remove-Item -ea 0 -force -path $toolsDir\*.exe
