c9297e852ca943f71941b43cc64cf0c302142c82
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-05-28 08:52:21 +0200

    [ultimmc] Import MultiMC package
    Source: https://github.com/sorashi/chocolatey-packages/tree/39fa65f4d4d0ee8e050ccf45b23d8ca1c2d1b9f5/manual/multimc
    
    Co-authored-by: Dennis Prazak <prazak.dennis@gmail.com>

---
+++ multimc/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://github.com/MultiMC/MultiMC5/releases/download/0.6.13/mmc-stable-win32.zip'
+$toolsDir   = "$(Get-ToolsLocation)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'multimc*'
+  checksum      = '27573d52e735ec1030a35a723eacb211513757930a99f8178a65d38407008a33'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Host "Creating Start Menu and Desktop shortcuts"
+$startmenu = Join-Path $env:programdata "Microsoft\Windows\Start Menu\Programs"
+Install-ChocolateyShortcut -ShortcutFilePath $(Join-Path $startmenu "MultiMC.lnk") -TargetPath $(Join-Path $toolsDir "MultiMC\MultiMC.exe")
+$desktop = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyShortcut -ShortcutFilePath $(Join-Path $desktop "MultiMC.lnk") -TargetPath $(Join-Path $toolsDir "MultiMC\MultiMC.exe")
---
+++ multimc/tools/chocolateyuninstall.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Removing Start Menu and Desktop shortcuts"
+$StartMenuShortcut = Join-Path $env:programdata "Microsoft\Windows\Start Menu\Programs\MultiMC.lnk"
+$DesktopShortcut = Join-Path $([Environment]::GetFolderPath("Desktop")) "MultiMC.lnk"
+if (Test-Path $StartMenuShortcut) 
+{
+  Remove-Item $StartMenuShortcut
+}
+if (Test-Path $DesktopShortcut)
+{
+  Remove-Item $DesktopShortcut
+}
+
+$multimcdir = Join-Path "$(Get-ToolsLocation)" MultiMC
+if(Test-Path $multimcdir)
+{
+  Write-Host "Your instances will be deleted (if they live in the MultiMC directory: $multimcdir)" -ForegroundColor Black -BackgroundColor Yellow
+  Write-Host "Ctrl-C to cancel" -ForegroundColor Black -BackgroundColor Yellow
+  timeout 10
+  Remove-Item -path "$multimcdir" -recurse
+}
