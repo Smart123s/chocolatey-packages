53ef8a886870be6b11fc659df602404027d8e1db
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-08-18 12:30:54 +0200

    [magisk-on-wsa] Delete package
    The repository was banned by GitHub.

--- magisk-on-wsa/tools/chocolateyuninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-
-$AppxPackage = Get-AppxPackage -Name 'MicrosoftCorporationII.WindowsSubsystemForAndroid'
-Remove-AppxPackage -Package $AppxPackage.PackageFullName
-
-Uninstall-ChocolateyZipPackage $env:ChocolateyPackageName "WSA*.zip"
