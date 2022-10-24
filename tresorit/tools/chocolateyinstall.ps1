0479f63b5fcfd293e4841133d096e15e96f5538b
Chocolatey <chocolatey@realdimensions.net>
2022-10-24 16:27:37 +0000

    AU: 1 updated - tresorit [skip ci] https://gist.github.com/be5bc050a10ff75f6fa0b8efe0cd35f6/dbde63875baccbd6f16523d24c08dbe5ff4f165d

--- tresorit/tools/chocolateyinstall.ps1
+++ tresorit/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
 
   softwareName  = 'Tresorit'
 
-  checksum64    = 'EEA47D1FBD58E1BA5D73C9B51EC69F18A563D6FA404BA4FF989B279EB6A81FE1'
+  checksum64    = '1F524DCDE7AE1964BD9B9DF273ED662C788253E15891BA84838CCABAB1238648'
   checksumType  = 'sha256'
 
   silentArgs    = '/s /v"/qn"'
