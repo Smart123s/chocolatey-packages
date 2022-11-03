f1c1e3735b2f5f5c46da4e8ffa6b99593a33974a
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-11-03 09:43:47 +0100

    [mullvad-app] Import package
    Source: https://github.com/watate/mullvad-app

---
+++ mullvad-app/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,2 @@
+﻿Get-Service -Name "Mullvad*" | Stop-Service
+Get-Process -Name "Mullvad*" | Stop-Process
---
+++ mullvad-app/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2022.5.exe'
+
+  checksum64    = 'ff5813895e951467e761e24d12d74f49f0b81573961b8388c305afc28c50e7c6'
+  checksumType64= 'sha256'
+
+  silentArgs    = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
