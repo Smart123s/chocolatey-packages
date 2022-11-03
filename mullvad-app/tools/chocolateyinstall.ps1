d63412f558a2df131b1a7a58da323daa63d1185d
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-11-03 20:55:09 +0100

    [mullvad-app] Add update.ps1

--- mullvad-app/tools/chocolateyinstall.ps1
+++ mullvad-app/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2022.5.exe'
+  url64         = 'https://mullvad.net/media/app/MullvadVPN-2022.5.exe'
 
   checksum64    = 'ff5813895e951467e761e24d12d74f49f0b81573961b8388c305afc28c50e7c6'
   checksumType64= 'sha256'
---
+++ mullvad-app/update.ps1
@@ -0,0 +1,28 @@
+Import-Module AU
+Add-Type -AssemblyName System.Web
+
+$releases = 'https://mullvad.net/en/download/windows/'
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"          
+            "(checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+		}
+	}
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum = Get-RemoteChecksum $Latest.url
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+    [regex]$regex  = '(?:.*Latest version: )(\d+.\d+)'
+    $version       = $regex.Matches($download_page.content).Value.Split(' ') | Select -Last 1
+    $url           = "https://mullvad.net/media/app/MullvadVPN-" + $version + ".exe"
+	
+    return @{ Version = $version; URL = $url }
+}
+
+Update-Package -ChecksumFor none
