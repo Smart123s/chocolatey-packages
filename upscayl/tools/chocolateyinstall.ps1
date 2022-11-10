348688feca381a68f9dbdc0f1b973873ac8e4fd6
Smart123s <28480228+Smart123s@users.noreply.github.com>
2022-11-10 09:35:43 +0100

    [upscayl] Initial commit

---
+++ upscayl/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Upscaly'
+
+  url64         = 'https://github.com/upscayl/upscayl/releases/download/v1.5.5/Upscayl-Setup-1.5.5.exe'
+  checksum64    = '68c7798a67d5481d96e6c0dbe0740cbc3bb7d2a7da23928aae0b9586e7e58bda'
+
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ upscayl/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+. $([System.IO.Path]::Combine("..", '_scripts', 'Get-GitHubLatestReleaseLinks.ps1'))
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"          
+            "(checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+    $download_page = Get-GitHubLatestReleaseLinks -User "upscayl" -Repository "upscayl"
+
+    $url64 = $download_page.links | ? href -match '.exe$' | % href | select -First 1
+    $version = ($url64 -split '/' | select -Last 1 -Skip 1).Replace('v', '')
+
+    @{
+       URL64   = 'https://github.com' + $url64
+       Version = $version
+    }
+}
+
+try {
+    update -ChecksumFor 64
+} catch {
+    $ignore = 'Unable to connect to the remote server'
+    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
