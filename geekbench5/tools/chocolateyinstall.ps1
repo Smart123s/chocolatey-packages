eab960040488113aca11425cc7e00cd0e8c3942e
Chocolatey <chocolatey@realdimensions.net>
2022-12-02 16:28:08 +0000

    AU: 2 updated - geekbench geekbench5 [skip ci] https://gist.github.com/be5bc050a10ff75f6fa0b8efe0cd35f6/1ee95e86055450192ef2a341940947d09a994b60

--- geekbench5/tools/chocolateyinstall.ps1
+++ geekbench5/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url64         = 'https://cdn.geekbench.com/Geekbench-5.4.5-WindowsSetup.exe'
+  url64         = 'https://cdn.geekbench.com/Geekbench-5.4.6-WindowsSetup.exe'
 
   softwareName  = 'Geekbench 5'
 
-  checksum64    = 'c58a8712c72277d197b378db41df0b0a2dfe65ca6082a443be37aeeea1a3915d'
+  checksum64    = '8e585c85371ff707cfee97109d03de5f5fe944e4f10f6ed36511cc659b8485f2'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
