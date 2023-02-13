91099fc24e07cacb1d4e3f510b90aaa59d1ea2cf
Chocolatey <chocolatey@realdimensions.net>
2023-02-13 16:28:06 +0000

    AU: 2 updated - jellyfin-media-player signalbackup-tools [skip ci] https://gist.github.com/be5bc050a10ff75f6fa0b8efe0cd35f6/aeab51a8adee519ddd0635247fec525c15604b83

--- jellyfin-media-player/tools/chocolateyinstall.ps1
+++ jellyfin-media-player/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Jellyfin Media Player'
 
-  url           = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.8.0/JellyfinMediaPlayer-1.8.0-windows-x86.exe'
-  checksum      = 'e362193d3789882d07f941ca98f99d7c9e628aa0d0b9bb06b1aca7eab56aa11c'
+  url           = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.8.1/JellyfinMediaPlayer-1.8.1-windows-x86.exe'
+  checksum      = '980c4789f49d56d7a1f43ceeecaddd6fb95a95608e3e3d02769ee52220bee618'
 
-  url64         = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.8.0/JellyfinMediaPlayer-1.8.0-windows-x64.exe'
-  checksum64    = '9d339212b330995db04439b138d99e865ae422d9f58f3bc180c95c5c48d8c45f'
+  url64         = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.8.1/JellyfinMediaPlayer-1.8.1-windows-x64.exe'
+  checksum64    = '829e797ecd9bbde5c362cad90730caed5e89406eb01d13913ffbafe019bbeabd'
 
   checksumType  = 'sha256'
 
