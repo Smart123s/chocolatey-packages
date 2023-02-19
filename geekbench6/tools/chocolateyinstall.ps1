e89485987f75b3215e8542bebd0439787c3a84a2
Smart123s <28480228+Smart123s@users.noreply.github.com>
2023-02-19 12:19:58 +0100

    [geekbench6] Initial commit

---
+++ geekbench6/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url64         = 'https://cdn.geekbench.com/k9ea2vqm/Geekbench-6.0.0-WindowsSetup.exe'
+
+  softwareName  = 'Geekbench 6'
+
+  checksum64    = '642acb59a87cf9fabef43231202e9f795357856d07862a5649a580f150a21c4b'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs
