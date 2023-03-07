f839ddb0d775feb4597df505f9eb3b01515ed10d
Chocolatey <chocolatey@realdimensions.net>
2023-03-07 16:28:04 +0000

    AU: 2 updated - mullvad-app signalbackup-tools [skip ci] https://gist.github.com/be5bc050a10ff75f6fa0b8efe0cd35f6/052496ca1a707a0a48ace74aa853f886717eb88f

--- mullvad-app/tools/chocolateyinstall.ps1
+++ mullvad-app/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64         = 'https://mullvad.net/media/app/MullvadVPN-2023.1.exe'
+  url64         = 'https://mullvad.net/media/app/MullvadVPN-2023.2.exe'
 
-  checksum64    = 'f3a9ff35d371c58820c9dd587943caa016f936f278c8bb36cc0297663cfd6c1a'
+  checksum64    = '78372ded0f058ea252192fd7298c38f1133a4a15e5b4736bfd8df52b6efc1b07'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
