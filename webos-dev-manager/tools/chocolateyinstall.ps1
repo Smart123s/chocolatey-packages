f9105fc8efe0e5a2935f1a9597736902225c0c3e
Chocolatey <chocolatey@realdimensions.net>
2023-04-04 08:33:56 +0000

    AU: 1 updated - webos-dev-manager [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/730f9902166e33e5c5c267509e86c8843ee7a972

--- webos-dev-manager/tools/chocolateyinstall.ps1
+++ webos-dev-manager/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  url           = 'https://github.com/webosbrew/dev-manager-desktop/releases/download/v1.11.4/webOS.Dev.Manager_1.11.4_x86_en-US.msi'
-  url64         = 'https://github.com/webosbrew/dev-manager-desktop/releases/download/v1.11.4/webOS.Dev.Manager_1.11.4_x64_en-US.msi'
+  url           = 'https://github.com/webosbrew/dev-manager-desktop/releases/download/v1.11.5/webOS.Dev.Manager_1.11.5_x86_en-US.msi'
+  url64         = 'https://github.com/webosbrew/dev-manager-desktop/releases/download/v1.11.5/webOS.Dev.Manager_1.11.5_x64_en-US.msi'
 
   softwareName  = 'webOS Dev Manager'
 
-  checksum      = 'bab801a1447509cb5147c8eeea82977cad7b5b471069b79b593128970da51442'
-  checksum64    = '32219094bffffc67670170ce0f97b4a1d367d977f3bdaed330dbc007b8df2576'
+  checksum      = 'aad9504f98077947fa3778216e0a5c283921ef3f69bcbb9c4a90bc968dc65701'
+  checksum64    = 'a8d5fdcc3ffd3102d83ffab316b192c2793a308004ffd8c869b9b0e3e9f69e22'
   checksumType  = 'sha256'
 
   silentArgs    = '/quiet'
