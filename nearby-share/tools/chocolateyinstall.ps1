f2cb01da89f0b51a5d423e67f2b6a1bc68bf2bfd
Smart123s <28480228+Smart123s@users.noreply.github.com>
2023-04-25 20:26:17 +0200

    [nearby-share] Initial commit

---
+++ nearby-share/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\helpers.ps1
+
+$version = '1.0.787.0'
+if ($version -eq (Get-NearbyShareVersion)) {
+  Write-Host "Nearby Share $version is already installed."
+  return
+}
+
+$packageArgs = @{
+  packageName            = 'Nearby Share'
+  fileType               = 'MSI'
+  url64                  = 'https://www.google.com/dl/release2/Nearby/acuoy3ppt3op4nop46bcpcgxyj4a_1.0.787.0/better_together.msi'
+  checksum64             = 'c135eb23fe4cebf4ba6a3b6f1c986aea366b050d0846f56c01c2cc7339217c0e'
+  checksumType64         = 'sha256'
+  silentArgs             = "/quiet /norestart"
+  validExitCodes         = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ nearby-share/update.ps1
@@ -0,0 +1,29 @@
+Import-Module AU
+
+$releases = 'https://tools.google.com/service/update2/'
+$releases_body = '<?xml version="1.0" encoding="UTF-8"?><request><os platform="win" version="10.0.22000.978" sp="" arch="x64"/><app appid="{232066FE-FF4D-4C25-83B4-3F8747CF7E3A}" version="" nextversion="" lang="en"><updatecheck/></app></request>'
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyinstall.ps1" = @{
+            "(url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.Version)'"
+		}
+	}
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases -Method Post -Body $releases_body
+    $app           = ([xml]$download_page.Content).gupdate.app.updatecheck
+    $version       = ($url -split '_' | select -Skip 1 -Last 1).Replace('/better', '')
+	
+    return @{
+        URL64      = $app.codebase
+        Checksum64 = $app.hash_sha256
+        Version    = $version
+    }
+
+}
+
+Update-Package -ChecksumFor none
