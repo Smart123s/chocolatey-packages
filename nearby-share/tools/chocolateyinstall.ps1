54484cb8772e88c545880f23b7b8988ef08ce842
Smart123s <28480228+Smart123s@users.noreply.github.com>
2023-04-25 20:34:05 +0200

    [nearby-share] Initial commit

---
+++ nearby-share/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url64         = 'https://dl.google.com/tag/s/appguid={232066FE-FF4D-4C25-83B4-3F8747CF7E3A}&usagestats=0&needsadmin=true/better_together/BetterTogetherSetup.exe'
+
+  softwareName  = 'Tresorit'
+
+  checksum64    = '98A93C17334A19EF86C153ECAE7BB7282668D9769115ABD313CDB7BFF1E4D499'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/s /v"/qn"'
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs
