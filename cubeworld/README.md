41cd5a559e180c1fb657c5731358860bc72a5e86
Smart123s <public@tmbpeter.com>
2023-08-17 10:17:50 +0200

    [cubeworld] Initial commit

---
+++ cubeworld/README.md
@@ -0,0 +1,15 @@
+## Self-Hosted notice
+
+This package is self-hosted because the game is downloaded from an unofficial archive.
+
+### Installation via Chocolatey
+
+1. Add my chocolatey repository as a source
+```
+choco source add -n smart123s-choco -s "https://nuget.cloudsmith.io/smart123s/choco/v2/"
+```
+
+2. Install package as usual
+```
+choco install cubeworld
+```
---
+++ cubeworld/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters = Get-PackageParameters
+
+### PARAMETERS
+If ($PackageParameters.InstallDir) {
+  $installDir = $PackageParameters.InstallDir
+} Else {
+  $installDir = "$toolsDir"
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  UnzipLocation = $installDir
+  
+  url           = 'https://archive.org/download/CubeWorldAlpha/cubeworld-20130624.zip'
+  checksum      = 'd17107e8ca2f7ab4415b1f8c0f36391f3acf5498a3d746ce79c69f0a7456d8bf'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$targetPath = Join-Path "$installDir" "Cube World\Cube.exe"
+
+# Add StartMenu shortcut
+If (-Not( $PackageParameters.NoStartMenuShortcut )) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
+  $programsFilePath = Join-Path "$programsPath" "Cube World.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath" -WorkingDirectory "$installDir\Cube World" -IconLocation "$installDir\cubeworld.ico"
+}
+
+# Add Desktop shortcut
+If (-Not( $PackageParameters.NoDesktopShortcut )) {
+  $programsPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+  $programsFilePath = Join-Path "$programsPath" "Cube World.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath" -WorkingDirectory "$installDir\Cube World" -IconLocation "$installDir\cubeworld.ico"
+}
+
