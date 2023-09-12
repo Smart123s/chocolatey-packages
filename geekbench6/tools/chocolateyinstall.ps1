bf656636aecee520578827502f9482324bef90c8
Chocolatey <chocolatey@realdimensions.net>
2023-09-12 16:28:15 +0000

    AU: 2 updated - geekbench geekbench6 [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/153f5b2e3823c489a11816feab9de7a66a9a90c3

--- geekbench6/tools/chocolateyinstall.ps1
+++ geekbench6/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url64         = 'https://cdn.geekbench.com/Geekbench-6.1.0-WindowsSetup.exe'
+  url64         = 'https://cdn.geekbench.com/Geekbench-6.2.0-WindowsSetup.exe'
 
   softwareName  = 'Geekbench 6'
 
-  checksum64    = '43543bf8a2594f0f46747f0cbd2102e8927fe92f96c14f27c80dfecffcf9ccbb'
+  checksum64    = '42dfe80ad174668eee591af41d0f887b158413acf461cd09dce64e2ad7397357'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
