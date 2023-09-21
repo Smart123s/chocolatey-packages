d9c46d06d77df05f64e583d4939dffbc40977681
Chocolatey <chocolatey@realdimensions.net>
2023-09-21 16:27:52 +0000

    AU: 2 updated - signalbackup-tools upscayl [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/d2a7e760e6a57d7fc9c3ccfe6413efe320ba49e5

--- upscayl/tools/chocolateyinstall.ps1
+++ upscayl/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Upscaly'
 
-  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.8.1/upscayl-2.8.1-win.exe'
-  checksum64    = '8dee28fe129980a101d6ee6c7ee422927573abe8b23dd1889c80b22dc711ece3'
+  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.8.5/upscayl-2.8.5-win.exe'
+  checksum64    = '7df8785d5ce586564cd654df8adbadf466f40cf5e8788af756d5015e2dd5ac6c'
 
   checksumType  = 'sha256'
 
