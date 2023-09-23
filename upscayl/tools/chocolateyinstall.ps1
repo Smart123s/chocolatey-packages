81f5a56c09209a0330921d0825b604eda9fe8892
Chocolatey <chocolatey@realdimensions.net>
2023-09-23 00:27:53 +0000

    AU: 1 updated - upscayl [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/c55a8691c40e666ebac2726b36085e8809c0bd5f

--- upscayl/tools/chocolateyinstall.ps1
+++ upscayl/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Upscaly'
 
-  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.8.5/upscayl-2.8.5-win.exe'
-  checksum64    = '7df8785d5ce586564cd654df8adbadf466f40cf5e8788af756d5015e2dd5ac6c'
+  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.8.6/upscayl-2.8.6-win.exe'
+  checksum64    = 'b69815d918f6ce89e218eceab16cb5262dbb64845f5936280b77a9312be84361'
 
   checksumType  = 'sha256'
 
