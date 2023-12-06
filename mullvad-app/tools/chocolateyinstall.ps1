7a7d78f1b1ed7e562ea77befc2cea075e05afa21
Chocolatey <chocolatey@realdimensions.net>
2023-12-06 16:28:04 +0000

    AU: 1 updated - mullvad-app [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/24f467a9fe32072178815cc821fb6701a10cdc8d

--- mullvad-app/tools/chocolateyinstall.ps1
+++ mullvad-app/tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64         = 'https://mullvad.net/media/app/MullvadVPN-2023.5.exe'
+  url64         = 'https://mullvad.net/media/app/MullvadVPN-2023.6.exe'
 
-  checksum64    = '60589fcbeb1c26ec028be1ee1f1d8ee105b0262f5ec9daa841cb016c1980283d'
+  checksum64    = '1a212857d3edcfe44cfdf0ac50db27a1fb325721f7fd9aa9a1e8fcc8b4bda64c'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
