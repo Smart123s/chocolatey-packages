d6bb46c8aad53eed1f81297e65f649259704f054
Chocolatey <chocolatey@realdimensions.net>
2024-01-19 00:27:55 +0000

    AU: 1 updated - nearby-share [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/29b60c952b05a61dd5db72bfd4962c8482fdbfc9

--- nearby-share/tools/chocolateyinstall.ps1
+++ nearby-share/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '1.0.1417.0'
+$version = '1.0.1439.0'
 if ($version -eq (Get-NearbyShareVersion)) {
   Write-Host "Nearby Share $version is already installed."
   return
@@ -10,8 +10,8 @@ if ($version -eq (Get-NearbyShareVersion)) {
 $packageArgs = @{
   packageName            = 'Nearby Share'
   fileType               = 'MSI'
-  url64                  = 'http://dl.google.com/release2/Nearby/lzfyfffdbxz5d77pmqebsehaau_1.0.1417.0/better_together.msi'
-  checksum64             = '8abcd92559bd3e367c1f787ea5a4bde25b9ca3b4c9d868707f49435a92177500'
+  url64                  = 'https://edgedl.me.gvt1.com/edgedl/release2/Nearby/lbwkksm5ue76nax4xzsqjohtl4_1.0.1439.0/better_together.msi'
+  checksum64             = '6e572d2f665a9e5080c443deba366a5536fb350bff94e1af98af7d01225be5e2'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart"
   validExitCodes         = @(0)
