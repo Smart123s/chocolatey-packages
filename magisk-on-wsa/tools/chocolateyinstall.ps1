6cdea2142bb9e28892667259c73fc067ec4442b9
Smart123s <public@tmbpeter.com>
2024-03-12 10:13:03 +0100

    bump: magisk-on-wsa to 2311.40000.5

--- magisk-on-wsa/tools/chocolateyinstall.ps1
+++ magisk-on-wsa/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   UnzipLocation  = "$toolsDir"
 
-  url64         = 'https://github.com/YT-Advanced/WSA-Script/releases/download/v2023-11-10/WSA_2309.40000.10.0_x64_Release-Nightly-with-Magisk-26.4-stable-MindTheGapps-13.0-as-Pixel-5-RemovedAmazon.7z'
-  checksum64    = 'ab97e97533b5751a697fc90872d6b54b0a101eede6095628ea39f9747f17fd03'
+  url64         = 'https://github.com/YT-Advanced/WSA-Script/releases/download/v2024-02-06/WSA_2311.40000.5.0_x64_Release-Nightly-with-Magisk-27.0-stable-MindTheGapps-13.0-as-Pixel-5-RemovedAmazon.7z'
+  checksum64    = '8da8cfbd4b812a7211e47cdbe18132ce21ab50ed9d3700b069fb9a50f21729a5'
 
   checksumType  = 'sha256'
 
