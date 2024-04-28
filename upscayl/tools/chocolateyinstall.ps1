890804baf7a7901652b9fa0055cb6f34818151a5
Smart123s <public@tmbpeter.com>
2024-04-28 09:41:51 +0200

    [upscayl] Fix checksum

--- upscayl/tools/chocolateyinstall.ps1
+++ upscayl/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   softwareName  = 'Upscaly'
 
   url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.11.0/upscayl-2.11.0-win.exe'
-  checksum64    = 'f526239d7121a41bec97e8fd7da89362b8f33d09f4e087afe63cc4164da94af1'
+  checksum64    = '8b715b2bbd7d3edd8e8dbcd3a662ea5ed7427fbab23a5e86605068c88e836258'
 
   checksumType  = 'sha256'
 
