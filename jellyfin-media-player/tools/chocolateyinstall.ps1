cfac37bfc7ea3e1a1183542321de5eff0b88cabc
Chocolatey <chocolatey@realdimensions.net>
2024-06-07 08:28:32 +0000

    AU: 1 updated - jellyfin-media-player [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/f511034edcd4f3f18ce2f0ef0bab30963f85991f

--- jellyfin-media-player/tools/chocolateyinstall.ps1
+++ jellyfin-media-player/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Jellyfin Media Player'
 
-  url           = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.10.1/JellyfinMediaPlayer-1.10.1-windows-x86.exe'
-  checksum      = '4f5a1659052452b44054a75ac7e52d540e2fe7443a070062840d7bcd30eb1816'
+  url           = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.11.0/JellyfinMediaPlayer-1.11.0-windows-x86.exe'
+  checksum      = '116ce71910d4fbb0b492766cffbdb274bb312a6d84cd977c9aa5229ee3c7c91f'
 
-  url64         = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.10.1/JellyfinMediaPlayer-1.10.1-windows-x64.exe'
-  checksum64    = '56613a23e532a1aa39ff0b259d13fbc8427aac797ff06700017eb464bff34d03'
+  url64         = 'https://github.com/jellyfin/jellyfin-media-player/releases/download/v1.11.0/JellyfinMediaPlayer-1.11.0-windows-x64.exe'
+  checksum64    = 'ac3095aab00bb99336f8a3bd30e89cb54b2c59e523c7da1d735bb2ad81f2cded'
 
   checksumType  = 'sha256'
 
