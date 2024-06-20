2dc63fad6935c13471af28c41801a3b8c5bbf20c
Smart123s <public@tmbpeter.com>
2024-06-20 16:01:15 +0200

    nearby-share: rename to quick-share

--- quick-share/tools/helpers.ps1
+++ quick-share/tools/helpers.ps1
@@ -2,6 +2,7 @@ function Get-NearbyShareVersion() {
   $root   = 'HKLM:\SOFTWARE\Google\Update\Clients'
   $root64 = 'HKLM:\SOFTWARE\Wow6432Node\Google\Update\Clients'
   foreach ($r in $root,$root64) {
+    # This is still not Quick Share on my PC
     $gcb = Get-ChildItem $r -ea 0 | Where-Object { (Get-ItemProperty $_.PSPath).name -eq 'Nearby Share' }
     if ($gcb) { return $gcb.GetValue('pv') }
   }
