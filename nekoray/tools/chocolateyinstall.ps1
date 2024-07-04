f2edfae7cea32716828a3cbf8bc1e409ba4d93dd
Smart123s <public@tmbpeter.com>
2024-07-04 22:43:15 +0200

    [nekoray] Initial commit

---
+++ nekoray/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+$packageArgs = @{
+    packageName    = "$env:chocolateyPackageName"
+    destination    = $toolsDir
+    FileFullPath64 = Join-Path $toolsDir 'nekoray.zip'
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -ea 0 -force -path $toolsDir\*.zip
+
+$targetPath = Join-Path (Join-Path $toolsDir "nekoray") "nekoray.exe"
+
+# Add StartMenu shortcut
+If (-Not( $PackageParameters.NoStartMenuShortcut )) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\nekoray.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath"
+}
+
+# Add Desktop shortcut
+If (-Not( $PackageParameters.NoDesktopShortcut )) {
+  $programsPath = "$Env:USERPROFILE\Desktop\nekoray.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath"
+}
---
+++ nekoray/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Remove start menu shortcut
+$programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\nekoray.lnk"
+if (Test-Path "$programsPath") { Remove-Item "$programsPath" }
+
+# Remove desktop icon
+$shortcutFilePath = "$Env:USERPROFILE\Desktop\nekoray.lnk"
+if (Test-Path "$shortcutFilePath") { Remove-Item "$shortcutFilePath" }
