7fb3e552b4a9683ccf2fbb57dfe40429df3fec95
Chocolatey <chocolatey@realdimensions.net>
2024-10-30 08:28:26 +0000

    AU: 1 updated - quick-share [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/624c0b9eb91c7d12cb2843fbb7ae47b272aabfdd

--- quick-share/tools/chocolateyinstall.ps1
+++ quick-share/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '1.0.1939.4'
+$version = '1.0.2002.2'
 if ($version -eq (Get-QuickShareVersion)) {
   Write-Host "Quick Share $version is already installed."
   return
@@ -10,8 +10,8 @@ if ($version -eq (Get-QuickShareVersion)) {
 $packageArgs = @{
   packageName            = 'Quick Share from Google'
   fileType               = 'MSI'
-  url64                  = 'https://dl.google.com/release2/Nearby/p6ywwl6cbdf47k5snmemcfcpcm_1.0.1939.4/better_together.msi'
-  checksum64             = 'd0950d9f530ad37e2d35e069962d1fa19e6694e69ee660b5f92a20eef7ede8d5'
+  url64                  = 'https://www.google.com/dl/release2/Nearby/n5wvbxjbele4gfuhbyviiyftdm_1.0.2002.2/better_together.msi'
+  checksum64             = '1c8c66116f604026564e5d342057d29787ac805e83d97564dc38db710234b833'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart"
   validExitCodes         = @(0)
