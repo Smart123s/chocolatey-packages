f54014b18eb6a817fdb2f0ec3852ceb911b9dee0
Péter Tombor <public@tmbpeter.com>
2025-01-09 21:11:25 +0100

    [upscaly] Fix version number for v2.15.0

--- upscayl/tools/chocolateyinstall.ps1
+++ upscayl/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   softwareName  = 'Upscaly'
 
-  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.1-win.exe'
-  checksum64    = '1ffad9e18ac4fd6ad3731ad708b13a8d60214ddd6a2764444e36df2d7d716acf'
+  url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-win.exe'
+  checksum64    = '203e4479c37547d1c5492a7bf2cbf98c6a40a61a9276eadacf04299e36f8a6ca'
 
   checksumType  = 'sha256'
 
