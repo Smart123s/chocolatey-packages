84dc92654e65447f5e857bd3b95df5d99d0b0eae
Péter Tombor <public@tmbpeter.com>
2025-03-30 11:49:41 +0200

    [geekbench-ai] Initial commit

---
+++ geekbench-ai/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  url64         = 'https://cdn.geekbench.com/GeekbenchAI-1.3.0-WindowsSetup.exe'
+
+  softwareName  = 'Geekbench AI'
+
+  checksum64    = '2047e935e1370e1f0ceb3023d1940a4e9e796a17bdc7e3435db841031b1ea65a'
+  checksumType  = 'sha256'
+
+  silentArgs    = '/S'
+  validExitCodes= @(0, 3010, 1641)
+
+}
+
+Install-ChocolateyPackage @packageArgs
