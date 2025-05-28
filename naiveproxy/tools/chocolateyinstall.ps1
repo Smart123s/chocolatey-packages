333bbb3dd206522760c12fda9c58567c66a3b7cb
Chocolatey <chocolatey@realdimensions.net>
2025-05-28 00:28:18 +0000

    AU: 1 updated - naiveproxy [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/e8e7a51eea5c6a68b36f4fc979b819a867b223fa

--- naiveproxy/tools/chocolateyinstall.ps1
+++ naiveproxy/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = 'https://github.com/klzgrad/naiveproxy/releases/download/v136.0.7103.44-1/naiveproxy-v136.0.7103.44-1-win-x86.zip'
-  url64         = 'https://github.com/klzgrad/naiveproxy/releases/download/v136.0.7103.44-1/naiveproxy-v136.0.7103.44-1-win-x64.zip'
+  url           = 'https://github.com/klzgrad/naiveproxy/releases/download/v137.0.7151.44-1/naiveproxy-v137.0.7151.44-1-win-x86.zip'
+  url64         = 'https://github.com/klzgrad/naiveproxy/releases/download/v137.0.7151.44-1/naiveproxy-v137.0.7151.44-1-win-x64.zip'
 
-  checksum      = 'c78d638d5413444f49b0784695644d3deba5611392c8de90b086fa3fe34edc04'
-  checksum64    = '09ff6651a6be4472ef3c4dac2eea3bbf90bb5a49419a938503990a8937aec9f1'
+  checksum      = '55a20cc3a6d5ede87f7c0d907d28c317539d36cec6fdfd9a3437a94df9a37fb9'
+  checksum64    = 'd54a70516c3b4adec9c595ec6f605a78890fb3e4245bf88401ac154cfd3a26d8'
   checksumType  = 'sha256'
 
   silentArgs    = '/quiet'
