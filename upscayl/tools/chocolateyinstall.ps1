90122308890e70423b67ac5e388ffad739bc4961
Péter Tombor <public@tmbpeter.com>
2025-08-04 13:48:22 +0200

    [upscayl] fix sha256

--- upscayl/tools/chocolateyinstall.ps1
+++ upscayl/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   softwareName  = 'Upscaly'
 
   url64         = 'https://github.com/upscayl/upscayl/releases/download/v2.15.0/upscayl-2.15.0-win.exe'
-  checksum64    = '203e4479c37547d1c5492a7bf2cbf98c6a40a61a9276eadacf04299e36f8a6ca'
+  checksum64    = '5dd7f58abafae2907fede489324a074cb6959a93c5714f3d0609650ab942ff3c'
 
   checksumType  = 'sha256'
 
