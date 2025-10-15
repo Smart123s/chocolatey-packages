c11f0a6c1b7d87ae0390f7cadeb6d13aa6719d33
Chocolatey <chocolatey@realdimensions.net>
2025-10-15 16:29:18 +0000

    AU: 2 updated - modrinth-app signalbackup-tools [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/3086e7876e6965b92b5f99682df8bc4c37b74186

--- modrinth-app/tools/chocolateyinstall.ps1
+++ modrinth-app/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url64         = 'https://launcher-files.modrinth.com/versions/0.10.10/windows/Modrinth%20App_0.10.10_x64-setup.exe'
+  url64         = 'https://launcher-files.modrinth.com/versions/0.10.11/windows/Modrinth%20App_0.10.11_x64-setup.exe'
 
   softwareName  = 'Modrinth App'
 
-  checksum64    = 'f2a56b2aafb581bc8d29db3cac735babebfb1ac40c811b3668ae65f724bee9b0'
+  checksum64    = '3604febc8269f48914d5fbae9eabc228e1517750c54171111cca77d6276d629f'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
