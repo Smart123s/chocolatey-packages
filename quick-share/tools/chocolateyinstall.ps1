76038b812b7694c70c83d263517b115305563241
Chocolatey <chocolatey@realdimensions.net>
2025-11-07 00:29:10 +0000

    AU: 1 updated - quick-share [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/1d66fe33f9dd8648dbe685727c6d4baf4518ff63

--- quick-share/tools/chocolateyinstall.ps1
+++ quick-share/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 . $toolsPath\helpers.ps1
 
-$version = '1.0.2351.1'
+$version = '1.0.2399.1'
 if ($version -eq (Get-QuickShareVersion)) {
   Write-Host "Quick Share $version is already installed."
   return
@@ -10,8 +10,8 @@ if ($version -eq (Get-QuickShareVersion)) {
 $packageArgs = @{
   packageName            = 'Quick Share from Google'
   fileType               = 'MSI'
-  url64                  = 'https://edgedl.me.gvt1.com/edgedl/release2/Nearby/nzycihmex74ey4qip3kn2navfq_1.0.2351.1/better_together.msi'
-  checksum64             = 'bbba569461c8339acd7b23aec6a21095abb955db3cccbe91af26efcca07e39a9'
+  url64                  = 'https://edgedl.me.gvt1.com/edgedl/release2/Nearby/acxp4o2gkafyjglomcjnbw3hnfxq_1.0.2399.1/better_together.msi'
+  checksum64             = 'f1db6539b9f213d193602b9387dbe394fd7247c7e1acae30ba208c5f84f453f2'
   checksumType64         = 'sha256'
   silentArgs             = "/quiet /norestart"
   validExitCodes         = @(0)
