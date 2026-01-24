e19fb2e24b60ce837407f979218c0e612aa6d3a0
Péter Tombor <public@tmbpeter.com>
2026-01-24 11:41:06 +0100

    [mega-mario] Initial commit

---
+++ mega-mario/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 
+
+$packageArgs = @{
+    packageName    = "$env:chocolateyPackageName"
+    destination    = $toolsDir
+    FileFullPath64 = Join-Path $toolsDir 'MegaMario_v1.7_full.zip'
+}
+
+Get-ChocolateyUnzip @packageArgs
+Remove-Item -ea 0 -force -path $toolsDir\*.zip
+
+$targetPath = Join-Path $toolsDir "MARIO.exe"
+
+# Add StartMenu shortcut
+If (-Not( $PackageParameters.NoStartMenuShortcut )) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Mega Mario.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath" -WorkingDirectory $toolsDir
+}
+
+# Add Desktop shortcut
+If (-Not( $PackageParameters.NoDesktopShortcut )) {
+  $programsPath = "$Env:USERPROFILE\Desktop\Mega Mario.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsPath" -targetPath "$targetPath" -WorkingDirectory $toolsDir
+}
---
+++ mega-mario/tools/chocolateyuninstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Remove start menu shortcut
+$programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Mega Mario.lnk"
+if (Test-Path "$programsPath") { Remove-Item "$programsPath" }
+
+# Remove desktop icon
+$shortcutFilePath = "$Env:USERPROFILE\Desktop\Mega Mario.lnk"
+if (Test-Path "$shortcutFilePath") { Remove-Item "$shortcutFilePath" }
