f1a65bbd69f22886853625165d538df8b10df852
Chocolatey <chocolatey@realdimensions.net>
2026-05-03 00:28:40 +0000

    AU: 2 updated - modrinth-app naiveproxy [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/3551199554ace65e3bdea7e4cd5b4de034cc2013

--- modrinth-app/tools/chocolateyinstall.ps1
+++ modrinth-app/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url64         = 'https://launcher-files.modrinth.com/versions/0.13.7/windows/Modrinth%20App_0.13.7_x64-setup.exe'
+  url64         = 'https://launcher-files.modrinth.com/versions/0.13.8/windows/Modrinth%20App_0.13.8_x64-setup.exe'
 
   softwareName  = 'Modrinth App'
 
-  checksum64    = '0129cc995eb0be8b1a5506b73c1829a383e433d603b00728aac03bf593b4cacc'
+  checksum64    = '56e9245703ae9c2c10bc20ebafc5cb4bc51e8efdf6563242a3a942ac4ea0cd74'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
--- naiveproxy/tools/chocolateyinstall.ps1
+++ naiveproxy/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = 'https://github.com/klzgrad/naiveproxy/releases/download/v147.0.7727.49-1/naiveproxy-v147.0.7727.49-1-win-x86.zip'
-  url64         = 'https://github.com/klzgrad/naiveproxy/releases/download/v147.0.7727.49-1/naiveproxy-v147.0.7727.49-1-win-x64.zip'
+  url           = 'https://github.com/klzgrad/naiveproxy/releases/download/v148.0.7778.96-2/naiveproxy-v148.0.7778.96-2-win-x86.zip'
+  url64         = 'https://github.com/klzgrad/naiveproxy/releases/download/v148.0.7778.96-2/naiveproxy-v148.0.7778.96-2-win-x64.zip'
 
-  checksum      = 'd6440d9a1a80ba11bcb9238c73243542f10697edfcacf8e5bbc6e1458fc0dcde'
-  checksum64    = '2de2cf78bec5d1cd58e5f57d1aa3b6c03b62039aaff1937f427a2dc466b46cbd'
+  checksum      = 'c03fea6905430d0b4a3fbb1fb20d160605b31f6272f17a7bc8dff82345707652'
+  checksum64    = '0dfcbe6d3d44ebf2c6ffea1e2b9ef40324888845cef9202fbdb3e2bce09e3ddc'
   checksumType  = 'sha256'
 
   silentArgs    = '/quiet'
