0bb6a8b27fe05c60f627dc4a92b6c38a21c73bc0
Chocolatey <chocolatey@realdimensions.net>
2026-05-16 00:30:16 +0000

    AU: 1 updated - hytale-launcher [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/b56606030d927756da43cc2a6695c0b9c49494b7

--- hytale-launcher/tools/chocolateyinstall.ps1
+++ hytale-launcher/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
 
   softwareName  = 'Hytale Launcher'
 
-  checksum64    = 'E05FCECBF9ED0241975530DFDFC14D62B682D111AEF600A3EFFF960D431C9490'
+  checksum64    = '0E1F96DFD54753D9A542F7C15592A850F6AE107EB3B3E1F86914F64FFB1EDD82'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
