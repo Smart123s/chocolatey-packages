3220b573fd529e2a8b0b6661c28536f863417225
Chocolatey <chocolatey@realdimensions.net>
2026-05-26 16:30:45 +0000

    AU: 1 updated - flaresolverr [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/1aefc46e93fdc0f156235e2589d8094455b29664

--- flaresolverr/tools/chocolateyinstall.ps1
+++ flaresolverr/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definitio
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url64         = 'https://github.com/FlareSolverr/FlareSolverr/releases/download/v3.4.6/flaresolverr_windows_x64.zip'
-  checksum64    = 'ffb4fb1395b353577e5ac4cd1674a166353ecec9710b5c5223faabba32d3cab4'
+  url64         = 'https://github.com/FlareSolverr/FlareSolverr/releases/download/v3.5.0/flaresolverr_windows_x64.zip'
+  checksum64    = '76a6c3e43af7de3827b2ea1badf2bb607f664caa445cef3f14d624b421f7e01e'
   checksumType  = 'sha256'
   UnzipLocation = $toolsDir
 }
