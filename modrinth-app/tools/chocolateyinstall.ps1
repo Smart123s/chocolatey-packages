bd77aa3fedbc03a6dee4d87453e9e110ed0554a5
Chocolatey <chocolatey@realdimensions.net>
2026-09-16 00:28:57 +0000

    AU: 1 updated - modrinth-app [skip ci] https://gist.github.com/Smart123s/be5bc050a10ff75f6fa0b8efe0cd35f6/7064e90e1eeaa26e3469cc0450d7c31e43bb1dac

--- modrinth-app/tools/chocolateyinstall.ps1
+++ modrinth-app/tools/chocolateyinstall.ps1
@@ -3,11 +3,11 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-  url64         = 'https://launcher-files.modrinth.com/versions/0.21.2/windows/Modrinth%20App_0.21.2_x64-setup.exe'
+  url64         = 'https://launcher-files.modrinth.com/versions/0.21.3/windows/Modrinth%20App_0.21.3_x64-setup.exe'
 
   softwareName  = 'Modrinth App'
 
-  checksum64    = '8805c78c233a494bf04f70fcd4412f36bc70e7f1a374b9540843e48175516b04'
+  checksum64    = 'e3c5c46391051c6f980f29009fe52b9b907709f8ad690b241ca38fef6be7fcb9'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
